ecc7d50dc0850255f72386a0a791d4ec066cd9b8
Henrique Lima <henriquelokb@gmail.com>
2022-06-23 14:46:58 -0300

    criado receita

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,10 @@
 const botaoGerenciar = document.getElementById("botao-gerenciar");
+const botaoCadastro = document.getElementById("botao-cadastro");
 
 botaoGerenciar.onclick = function () {
 	location.href = "account.html";
+};
+
+botaoCadastro.onclick = function () {
+	location.href = "register.html";
 };
---
+++ assets/js/register.js
@@ -0,0 +1,5 @@
+const botaoCriaReceita = document.getElementById("botao-cria-receita");
+
+botaoCriaReceita.onclick = function() {
+	location.href = "dashboard.html";
+};
